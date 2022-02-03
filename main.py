ded4e2f44dd8a0e1f116e03b5f9d7da33cad0994
Billy Barnes <86270229+B4CKID@users.noreply.github.com>
2022-02-03 20:58:44 +0000

    Fully functional. Allows for limit guesses or infinite.

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ def stop_game():
   if start == 'y':
    print("Ok lets start again!")
    number_generator()
-   print(numbers3)
    start_game()
   elif start == 'n':
     y=2
@@ -34,8 +33,8 @@ def clues():
     print("you win!")
     stop_game()
     y=2
-  elif numbers3[0] == x[0] and numbers3[1] and x[1] or numbers3[1] == x[1] and numbers3[2] and x[2] or numbers3[0] == x[0] and numbers3[2] == x[2]:
-    print("You guess more than one correct!")   
+  elif numbers3[0] == x[0] and numbers3[1] and x[1] or numbers3[1] == x[1] and numbers3[2] and x[2] or numbers3[0] == x[0] and numbers3[2] == x[2] or numbers3[0] == x[1] or numbers3[0]==[2]or numbers3[1] == x[2] or numbers3[1]==[0]or numbers3[2] == x[1] or numbers3[2]==[0]:
+    print("You guessed more than one correct!")   
   elif numbers3[0] == x[0]:
     print("You have a match, try again!")
   elif numbers3[1] == x[1]:
@@ -51,7 +50,9 @@ def start_game():
   y=0
   while y != 2:
     player2 = input("What is your guess?: ")
-    if player2 > "999" or player2 < "":
+    if len(player2) > 3:
+      print("This input is not valid")
+    elif len(player2) <3:
       print("This input is not valid")
     else:
       try:
@@ -61,21 +62,28 @@ def start_game():
         print("This input is not valid") 
       
 number_generator()
-print(numbers3)
-start_game()
+# print(numbers3)
+# start_game()
 
 
 
 
-
-  
-# for x in range(7):
-#   if x == 6:
-#     print("You're out of guesses!")
-#     stop_game()
-#   player2 = input("What is your guess?: ")
-#   try:
-#     x = [int(x) for x in player2]
-#     clues()
-#   except ValueError:
-#     print("This input is not valid")
+#some game logic for limit of guesses 
+for x in range(7):
+  if x == 6:
+    print("You're out of guesses!")
+    stop_game()
+  player2 = input("What is your guess?: ")
+  if len(player2) > 3:
+    print("This input is not valid")
+  elif len(player2) <3:
+    print("This input is not valid")
+  else:
+    try:
+      x = [int(x) for x in player2]
+      clues()
+    except ValueError:
+      print("This input is not valid") 
+      
+#some game logic if you want player to know they guess more than one right with out them being in order.
+#  or numbers3[0] == x[1] or numbers3[0]==[2]or numbers3[1] == x[2] or numbers3[1]==[0]or numbers3[2] == x[1] or numbers3[2]==[0:
